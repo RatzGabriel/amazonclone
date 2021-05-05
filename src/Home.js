0d6fa114b65a206377fc3f78c2b0fddd40ece111
gabriel <gabrielratz84@gmai.com>
2021-05-05 07:25:02 +0200

    added Home and Product

---
+++ src/Home.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Home.css";
+import Product from "./Product";
+function Home() {
+  return (
+    <div className="home">
+      <img
+        className="home__image"
+        src="https://images-eu.ssl-images-amazon.com/images/G/03/digital/video/gateway/placement/launch/Voyagers/VYGRS_2021_GWBleedingHero_ENG_COVIDUPDATE_XSite_1500X600_PV_de-DE._CB670785129_.jpg"
+        alt=""
+      />
+      <Product
+        id={12345}
+        title="start"
+        price={11.96}
+        rating={5}
+        image="https://m.media-amazon.com/images/I/51PxsMGHM1L._SY346_.jpg"
+      />
+    </div>
+  );
+}
+
+export default Home;
